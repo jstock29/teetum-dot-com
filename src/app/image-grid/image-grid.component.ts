0b1529bc5b843d79fa61ff622f5bd0329a2c279e
Jared <jstock529@gmail.com>
2019-10-07 22:20:27 -0400

    update content and webmanifest

--- src/app/image-grid/image-grid.component.ts
+++ src/app/image-grid/image-grid.component.ts
@@ -16,6 +16,8 @@ export class ImageGridComponent implements OnInit {
 			'./assets/images/IMG_0692.PNG',
 			'./assets/images/IMG_0694.PNG',
 			'./assets/images/IMG_0695.PNG',
+			// './assets/images/IMG_0036.jpg',
+			'./assets/images/IMG_0037.jpg',
 			'./assets/images/0E9C8CEF-F833-4EF1-B0F0-45A1A2407444.JPG',
 			'./assets/images/IMG_2311.jpg',
 		]
